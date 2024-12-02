74266a8af0027e52c5f6eda2dc2a382354292337
frnxx <144995075+phrnxx@users.noreply.github.com>
2024-12-02 19:26:09 +0300

    db_config.py

--- Практична Робота 5 аналіз роботи запитів/db_config.py
+++ Практична Робота 5 аналіз роботи запитів/db_config.py
@@ -5,3 +5,6 @@ db_config = {
     'user': 'your_db_user',
     'password': 'your_db_password'
 }
+
+def get_database_url():
+    return f"postgresql://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['dbname']}"
